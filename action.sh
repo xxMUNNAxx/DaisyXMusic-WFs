50a14290a96cbbc9313312d7fc380c19f9829324
xxMUNNAxx <82759594+xxMUNNAxx@users.noreply.github.com>
2021-09-13 16:09:04 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,5 +1,5 @@
 
-git clone https://github.com/xxMUNNAxx/VeezMusic /root/tgvc
+git clone https://github.com/xxMUNNAxx/video-stream /root/tgvc
 cp tgvc/local.env /root/tgvc/local.env
 cd /root/tgvc
 docker build . -t fuk
